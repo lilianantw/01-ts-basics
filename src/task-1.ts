d8cc798c134c76f35cd05d871f3a043f16fbf331
liliana_toiunda <liliana.ntw@gmail.com>
2025-06-18 23:35:01 +0300

    Initial commit

---
+++ src/task-1.ts
@@ -0,0 +1,18 @@
+// Задача 1. Типізація скалярних значень (файл task-1.ts)
+
+// У цьому фрагменті коду оголошені три змінні з різними типами значень: текст, число і булеве значення.
+// const name = "Alice";
+// const age = 30;
+// const isOnline = true;
+
+// console.log(`Name: ${name}`);
+// console.log(`Age: ${age}`);
+// console.log(`Online: ${isOnline}`);
+
+const name: string = "Alice";
+const age: number = 30;
+const isOnline: boolean = true;
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Online: ${isOnline}`);
